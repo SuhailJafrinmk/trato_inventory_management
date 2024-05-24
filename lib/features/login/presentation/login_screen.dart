56365504d75bde4c1ae0af2e7ba0afabd1d328d1
suhailjafrin <suhailmk196@gmail.com>
2024-05-24 14:15:42 +0530

    before changing build gradle

--- lib/features/login/presentation/login_screen.dart
+++ lib/features/login/presentation/login_screen.dart
@@ -3,10 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trato_inventory_management/features/login/bloc/login_bloc.dart';
 import 'package:trato_inventory_management/utils/constants/colors.dart';
 import 'package:trato_inventory_management/utils/constants/icons.dart';
+import 'package:trato_inventory_management/utils/constants/regex.dart';
 import 'package:trato_inventory_management/utils/constants/text_styles.dart';
 import 'package:trato_inventory_management/widgets/app_textfield.dart';
 import 'package:trato_inventory_management/widgets/custom_button.dart';
-
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -16,23 +16,26 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController emailController=TextEditingController();
-  TextEditingController passwordController=TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-  final size=MediaQuery.of(context).size;
-  return BlocListener<LoginBloc, LoginState>(
-    listener: (context, state) {
-      if(state is LoginLoadedState){
-
-      }else if(state is LoginErrorState){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.message)));
-
-      }else if(state is LoginSuccessState){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Login Succesfull')));
-      }
-    },
-    child: Scaffold(
+    final size = MediaQuery.of(context).size;
+    final bloc = BlocProvider.of<LoginBloc>(context);
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is LoginLoadedState) {
+        } else if (state is LoginErrorState) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.message)));
+        } else if (state is LoginSuccessState) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text('Login Succesfull')));
+          Navigator.pushReplacementNamed(context, 'home_screen');
+        }
+      },
+      child: Scaffold(
         body: Stack(
           children: [
             Row(
@@ -55,71 +58,129 @@ class _LoginScreenState extends State<LoginScreen> {
               alignment: Alignment.center,
               child: SingleChildScrollView(
                 child: Container(
-                  width: size.width*.9,
-                  height: size.height*.7,
+                  width: size.width * .9,
+                  height: size.height * .7,
                   decoration: BoxDecoration(
-                    color: AppColors.primaryColor,
-                    borderRadius: BorderRadius.circular(20)
-                  ), 
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 30),
-                        child: Text('Welcome back',
-                        style: signInGreeting,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 60),
-                        child: Container(
-                         height: size.height * .5,
-                         width: size.width * .8,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(
-                              color: Colors.white
-                            )
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: BlocBuilder<LoginBloc, LoginState>(
+                    builder: (context, state) {
+                      if(state is LoginLoadedState){
+                        return Center(
+                          child: SizedBox(
+                            height: 20,
+                            width: 20,
+                            child: CircularProgressIndicator(),
                           ),
-                          child: Column(
-                            children: [
-                              AppTextfield(
-                                labelStyle: labeltextwhite,
-                                inputStyle: inputFieldTextWhite,
-                                obscureText: false,
-                                textEditingController:emailController,
-                                labelText: 'Email',
-                                width: size.width*.7,
-                                padding: 20,
-                                suffixIcon: AppIcons.visibility
+                        );
+                      }
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 30),
+                            child: Text(
+                              'Welcome back',
+                              style: signInGreeting,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 60),
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 20),
+                              height: size.height * .5,
+                              width: size.width * .8,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  border: Border.all(color: Colors.white)),
+                              child: Form(
+                                key: formkey,
+                                child: Column(
+                                  children: [
+                                    AppTextfield(
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter an email';
+                                          }
+                                          if (!RegexUtils.emailRegExp
+                                              .hasMatch(value)) {
+                                            return 'Please enter a valid email';
+                                          }
+                                          return null;
+                                        },
+                                        labelStyle: labeltextwhite,
+                                        inputStyle: inputFieldTextWhite,
+                                        obscureText: false,
+                                        textEditingController: emailController,
+                                        labelText: 'Email',
+                                        width: size.width * .7,
+                                        padding: 20,
+                                        suffixIcon: AppIcons.visibility),
+                                    AppTextfield(
+                                        validator: (value) {
+                                          if (value == null || value.isEmpty) {
+                                            return 'Please enter a password';
+                                          }
+                                          return null;
+                                        },
+                                        labelStyle: labeltextwhite,
+                                        inputStyle: inputFieldTextWhite,
+                                        obscureText: false,
+                                        textEditingController:
+                                            passwordController,
+                                        labelText: 'Password',
+                                        width: size.width * .7,
+                                        padding: 20),
+                                    SizedBox(
+                                      height: size.height * .04,
+                                    ),
+                                    CustomButton(
+                                      onTap: () {
+                                        if (formkey.currentState!.validate()) {
+                                          bloc.add(LoginButtonPressedEvent(
+                                              userEmail: emailController.text,
+                                              userPassword:
+                                                  passwordController.text));
+                                        }
+                                      },
+                                      height: size.height * .06,
+                                      width: size.width * .70,
+                                      elevation: 0,
+                                      color: AppColors.secondaryColor,
+                                      radius: 20,
+                                      child: Text(
+                                        'Submit',
+                                        style: buttonText,
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text(
+                                          'dont have an account',
+                                          style: minorText,
+                                        ),
+                                        TextButton(
+                                            onPressed: () {
+                                              Navigator.pushNamed(
+                                                  context, 'signup');
+                                            },
+                                            child: Text(
+                                              'SignUp',
+                                              style: textbutton,
+                                            ))
+                                      ],
+                                    )
+                                  ],
                                 ),
-                              AppTextfield(
-                                labelStyle: labeltextwhite,
-                                inputStyle: inputFieldTextWhite,
-                                obscureText: false,
-                                textEditingController: passwordController,
-                                labelText: 'Password',
-                                width: size.width*.7,
-                                padding: 20),
-                              SizedBox(height: size.height*.04,),
-                              CustomButton(
-                                onTap: () => Navigator.pushReplacementNamed(context, 'add_store'),
-                                height: size.height*.06,
-                                 width: size.width*.70,
-                                 elevation: 0,
-                                 color: AppColors.secondaryColor,
-                                 radius: 20,
-                                 child: Text('Submit',style: buttonText,),),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [Text('dont have an account',style: minorText,),TextButton(onPressed: (){
-                                 Navigator.pushNamed(context,'signup');
-                                }, child: Text('SignUp',style: textbutton,))],)
-                            ],
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                    ],
+                        ],
+                      );
+                    },
                   ),
                 ),
               ),
@@ -127,8 +188,6 @@ class _LoginScreenState extends State<LoginScreen> {
           ],
         ),
       ),
-  );
-
+    );
   }
-    
 }
--- lib/models/store_model.dart
+++ lib/models/store_model.dart
@@ -2,6 +2,28 @@ class StoreModel{
   final String storeName;
   final String location;
   final String contactInfo;
-  final String gsdId;
+  final String gstId;
   final String currency;
-  StoreModel({required this.storeName, required this.location, required this.contactInfo, required this.gsdId, required this.currency});}
+  StoreModel({required this.storeName, required this.location, required this.contactInfo, required this.gstId, required this.currency});
+  Map<String,dynamic>toMap(){
+    return {
+      'storeName':storeName,
+      'location':location,
+      'contactInfo':contactInfo,
+      'gstId':gstId,
+      'currency':currency,
+    };
+  }
+
+ factory StoreModel.fromMap(Map<String, dynamic> data) {
+    return StoreModel(
+      storeName: data['storeName'] ?? '',
+      location: data['location'] ?? '',
+      contactInfo: data['contactInfo'] ?? '',
+      gstId: data['gstId'] ?? '',
+      currency: data['currency'] ?? '',
+    );
+  }
+  
+  }
+
